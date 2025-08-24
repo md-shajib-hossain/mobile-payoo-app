cbf4da7ab01ab2842dda9c6e8cf7067b03aa2271
unknown <mdshajib5159@gmail.com>
2025-08-24 21:21:58 +0600

    grid section mouseover and mouse leave effect added

--- home.js
+++ home.js
@@ -1,15 +1,67 @@
-document.getElementById("btn-add-money").addEventListener("click", function () {
-  const bankAcc = 12345678910;
-  const pinNumber = 1234;
-  const amount = parseInt(document.getElementById("amount").value);
-  const amountConverted = parseInt(amount);
-  const pin = parseInt(document.getElementById("pin").value);
-  const bankAccNumber = parseInt(
-    document.getElementById("bank-acc-number").value
-  );
-  let taka = document.getElementById("current-balance").innerText;
-  if (bankAccNumber === bankAcc && pin === pinNumber) {
-  } else {
-    alert("invalid ");
-  }
-});
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const bank = document.getElementById("bank-name").value;
+    const accountNumber = parseInt(
+      document.getElementById("bank-acc-number").value
+    );
+    const amount = parseInt(document.getElementById("amount").value);
+
+    const pinNumber = parseInt(document.getElementById("pin").value);
+    const currentBalance = parseInt(
+      document.getElementById("current-balance").innerText
+    );
+
+    let newBalance = amount + currentBalance;
+    console.log(newBalance);
+    document.getElementById("current-balance").innerText = newBalance;
+  });
+
+//
+// toggling features
+document
+  .getElementById("add-money-feature")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-container").style.display = "block";
+    document.getElementById("cash-out-container").style.display = "none";
+
+    // document.getElementById("add-money-feature").style.backgroundColor = "red";
+  });
+
+document
+  .getElementById("cash-out-feature")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-container").style.display = "none";
+    document.getElementById("cash-out-container").style.display = "block";
+  });
+
+// document
+//   .getElementById("add-money-feature")
+//   .addEventListener("mouseover", function () {
+//     document.getElementById("add-money-feature").style.backgroundColor =
+//       "lightblue";
+//   });
+// document
+//   .getElementById("add-money-feature")
+//   .addEventListener("mouseleave", function () {
+//     document.getElementById("add-money-feature").style.backgroundColor =
+//       "white";
+//   });
+
+//  mouse over and mouse leave function
+function mouseOverOutEffect(id) {
+  document.getElementById(id).addEventListener("mouseover", function () {
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    document.getElementById(id).addEventListener("mouseleave", function () {
+      document.getElementById(id).style.backgroundColor = "white";
+    });
+  });
+}
+// function ends here
+mouseOverOutEffect("add-money-feature");
+mouseOverOutEffect("cash-out-feature");
+mouseOverOutEffect("transfer-money-feature");
+mouseOverOutEffect("get-bonus-feature");
+mouseOverOutEffect("pay-bill-feature");
+mouseOverOutEffect("transactions-feature");
